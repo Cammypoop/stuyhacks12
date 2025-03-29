91667cda70c370dcda4d21188cc0d06dfdf66e4e
schen2008 <schen200826@gmail.com>
2025-03-29 09:38:32 -0700

    initialized some stuff

--- Game.java
+++ Game.java
@@ -1,4 +1,13 @@
+import java.util.*;
+import java.io.*;
 public class Game{
+    public int[][] field;
+    //ask user for difficulty
+    // easy : 9x9, 10 mines
+    // intermediate : 16x16, 40 mines
+    // expert : 30x16, 99 mines
+    boolean live = true;
+    // if this boolean is false terminate program
     public static void main(String[] args) {
         
     }
