aea8959524822dcdc9325b52abf210d9cc267974
schen2008 <schen200826@gmail.com>
2025-03-29 09:42:09 -0700

    working on fieldgen

--- Game.java
+++ Game.java
@@ -6,9 +6,14 @@ public class Game{
     // easy : 9x9, 10 mines
     // intermediate : 16x16, 40 mines
     // expert : 30x16, 99 mines
+    int bombs; 
+    // initialize this number based on difficulty
     boolean live = true;
     // if this boolean is false terminate program
     public static void main(String[] args) {
+        
+    }
+    public void fieldgenerator(int bombs){
         
     }
 }
