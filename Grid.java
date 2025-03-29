eb5cf391acb67606ede60e9feef3cc8d7465f577
schen2008 <schen200826@gmail.com>
2025-03-29 10:12:23 -0700

    sdfdsf

--- Grid.java
+++ Grid.java
@@ -1,17 +1,21 @@
 import java.util.*;
 import java.io.*;
 public class Grid{
-    public Cell[][] field = new Cell[][];
+    public Cell[][] field;
     //ask user for difficulty
     // easy : 9x9, 10 mines
     // intermediate : 16x16, 40 mines
     // expert : 30x16, 99 mines 
     boolean live = true;
     // if this boolean is false terminate program
+    public Grid(int bombs, int x, int y){
+        field = new Cell[x][y];
+        fieldgenerator(bombs);
+    }
 
     public void fieldgenerator(int bombs){
         while(bombs > 0){
-            field.get(Math.random()*field.length).setBomb()
+            field.get(Math.random()*length(), Math.random()*field[0].length).setBomb();
         }
     }
 
@@ -19,6 +23,9 @@ public class Grid{
 
     public Cell get(int x, int y){
         return field[x][y];
+    }
+    public int length(){
+        return field.length;
     }
 
 }
