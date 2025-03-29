2bd3cb53db2c8176ed5e42f8fe7b6e1c8374afe9
schen2008 <schen200826@gmail.com>
2025-03-29 13:30:05 -0700

    starting to write run

--- Game.java
+++ Game.java
@@ -1,10 +1,40 @@
 import java.util.*;
+
+import org.w3c.dom.Text;
 public class Game {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome to Minesweeper!");
-        System.out.println("Enter the difficulty you would like to play: easy, medium or hard");
-        String difficulty = scan.nextLine();
-        Grid g = new Grid(difficulty);
+    // public static void main(String[] args) {
+    //     Scanner scan = new Scanner(System.in);
+    //     System.out.println("Welcome to Minesweeper!");
+    //     System.out.println("Enter the difficulty you would like to play: easy, medium or hard");
+    //     String difficulty = scan.nextLine();
+    //     Grid g = new Grid(difficulty);
+    // }
+
+    public static String userInput(Scanner in){
+      //Move cursor to prompt location
+
+      Text.go(29, 65);
+      //show cursor
+      Text.showCursor();
+      String input = in.nextLine();
+      Grid grid;
+
+      //clear the text that was written
+      //Text.clear();
+      return input;
+
+  }
+
+  public static void run(){
+    String input = "";
+    Scanner in = new Scanner(System.in);
+    System.out.println("Welcome to Minesweeper!");
+    while(!(input.equalsIgnoreCase("easy") || input.equalsIgnoreCase("intermediate") || input.equalsIgnoreCase("expert"))){
+        System.out.println("Enter the difficulty you would like to play: easy, intermediate, expert");
+        input = userInput(in);
     }
+    Grid grid = new Grid(input);
+    
+
+  }
 }
