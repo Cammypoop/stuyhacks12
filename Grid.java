33a80180f108e788fd6f8fbe6712e4de84fbfa69
Steven Wu <steveph945@gmail.com>
2025-03-29 11:50:25 -0400

    sdfd

--- Grid.java
+++ Grid.java
@@ -1,30 +1,31 @@
 import java.util.*;
 import java.io.*;
 public class Grid{
-    public Cell[][] field;
+    private Cell[][] field;
     //ask user for difficulty
     // easy : 9x9, 10 mines
     // intermediate : 16x16, 40 mines
     // expert : 30x16, 99 mines 
     boolean live = true;
     // if this boolean is false terminate program
-    public Grid(int bombs, int x, int y){
-        field = new Cell[x][y];
+    public Grid(int bombs, int r, int c){
+        field = new Cell[r][c];
         fieldgenerator(bombs);
     }
 
     public void fieldgenerator(int bombs){
-        while(bombs > 0){
+        int bombsLeftToPlace = bombs;
+        while(bombsLeftToPlace > 0){
             field.get(Math.random()*length(), Math.random()*field[0].length).setBomb();
         }
     }
 
     //accessor methods
 
-    public Cell get(int x, int y){
-        return field[x][y];
+    private Cell get(int r, int c){
+        return field[r][c];
     }
-    public int length(){
+    private int length(){
         return field.length;
     }
 
