04fec34dc7fdc75070953bf7b04fef3ced106740
Steven Wu <steveph945@gmail.com>
2025-03-29 13:06:02 -0400

    debugtoString

--- Cell.java
+++ Cell.java
@@ -2,12 +2,13 @@ public class Cell {
     private boolean isMine;
     private int numMines;
     private boolean flagged;
+    private boolean revealed;
 
-    public Cell(boolean isMine){
+    public Cell(boolean isMine) {
         this.isMine = isMine;
     }
 
-    public String toString() {
+    public String debugToString() {
         if (isMine) {
             return "*";
         } else if (flagged) {
@@ -19,24 +20,51 @@ public class Cell {
         }
     }
 
+    public String toString() {
+        if (revealed) {
+            if (isMine) {
+                return "*";
+            } else if (flagged) {
+                return "F";
+            } else if (numMines > 0) {
+                return Integer.toString(numMines);
+            } else {
+                return " ";
+            }
+        } else {
+            return "\\";
+        }
+    }
+
     public int getNumMines() {
         return numMines;
     }
-    public boolean getIsMine(){
+
+    public boolean getIsMine() {
         return isMine;
     }
 
-    public boolean getFlagged(){
+    public boolean getFlagged() {
         return flagged;
     }
 
-    public void setFlagged(boolean flagged){
+    public boolean getRevealed() {
+        return revealed;
+    }
+
+    public void setRevealed(boolean revealed) {
+        this.revealed = revealed;
+    }
+
+    public void setFlagged(boolean flagged) {
         this.flagged = flagged;
     }
+
     public void setMine(boolean isMine) {
         this.isMine = isMine;
     }
-    public void setNumMines(int newMines){
+
+    public void setNumMines(int newMines) {
         this.numMines = newMines;
     }
 }
