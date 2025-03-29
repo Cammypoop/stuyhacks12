68e355596009b498baa77a48bcb71cee4fb9ab63
Steven Wu <steveph945@gmail.com>
2025-03-29 12:52:52 -0400

    generator

--- Grid.java
+++ Grid.java
@@ -3,26 +3,38 @@ import java.io.*;
 public class Grid{
     private Cell[][] field;
     boolean live;
+    int totalBombs;
     //ask user for difficulty
     // easy : 9x9, 10 mines
     // intermediate : 16x16, 40 mines
     // expert : 30x16, 99 mines 
     // if this boolean is false terminate program
     public Grid(int bombs, int r, int c){
+        int totalBombs = bombs;
         live = true;
         field = new Cell[r][c];
+        for (int i = 0; i < r; i++){
+            for (int j = 0; j < c; j++){
+                field[i][j] = new Cell(false);
+            }
+        }
         fieldgenerator(bombs);
     }
 
     public void fieldgenerator(int bombs){
         while(bombs > 0){
-            int r = (int) Math.random() * field.length;
-            int c = (int) Math.random() * field[0].length;
+            int r = (int) (Math.random() * field.length);
+            // System.out.println(field.length); // debug
+            int c = (int) (Math.random() * field[0].length);
             Cell selectedCell = get(r,c);
-            if (selectedCell.getIsMine()){
+            if (!selectedCell.getIsMine()){
                 selectedCell.setMine(true);
             }
+            else{
+                continue;
+            }
             bombs--;
+            // System.out.println(toString()); // Debug
         }
     }
 
@@ -39,7 +51,7 @@ public class Grid{
 
     //accessor methods
 
-    private Cell get(int r, int c){
+    public Cell get(int r, int c){
         return field[r][c];
     }
 
--- Tester.java
+++ Tester.java
@@ -1,6 +1,7 @@
 public class Tester {
     public static void main(String[] args) {
         Grid g = new Grid(25, 10, 10);
+        // System.out.println(g.get(0,0));
         System.out.println(g.toString());
     }
 }
